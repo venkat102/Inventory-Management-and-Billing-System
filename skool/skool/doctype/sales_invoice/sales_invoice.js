bba108622a9ee7d71bd49e0082edd13581dd25f1
venkat102 <venkatesharunachalam659@gmail.com>
2021-02-28 22:20:09 +0530

    feat[sales invoice]: auto populate data feat[stock ledger]: create record on creating sales invoice

--- skool/skool/doctype/sales_invoice/sales_invoice.js
+++ skool/skool/doctype/sales_invoice/sales_invoice.js
@@ -2,7 +2,126 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Sales Invoice', {
-	// refresh: function(frm) {
+	onload:function(frm){
+		if(!cur_frm.doc.__unsaved){
+			frm.set_df_property('is_return', 'hidden', 1);
+			frm.refresh_field('is_return');
+		}
+	},
+	discount:function(frm){
+		var total = 0;
+		for (var i = 0; i<frm.doc.items.length; i++){
+			total += parseFloat(frm.doc.items[i].amount);
+		}
+		frm.set_value("amount", total);
+		frm.set_value("total_amount", total - parseFloat(frm.doc.discount))
+		frm.refresh_field("amount");
+		frm.refresh_field("total_amount");
+	},
+	is_return:function(frm){
+		if(frm.doc.is_return){
+			frm.set_df_property("invoice_no", 'hidden', 0);
+		}
+		else{
+			location.reload();
+		}
+	},
+	invoice_no:function(frm){
+		if(frm.doc.invoice_no){
+			if(frm.doc.invoice_no.includes('Returned')){
+				frappe.throw("Can not return a returned invoice");
+				frm.set_value("invoice_no",'');
+			}
+			frappe.call({
+				method:"skool.skool.doctype.sales_invoice.sales_invoice.check_returned",
+				args:{'invoice':frm.doc.invoice_no},
+				callback:function(res){		
+					if(!res.exc){	
+						if(!res.message){
+							frappe.call({
+								method:"skool.skool.doctype.sales_invoice.sales_invoice.get_data",
+								args:{'invoice': frm.doc.invoice_no},
+								callback:function(res){
+									frm.set_value("amount", res.message[0][0]);
+									frm.set_value("discount", res.message[0][1]);
+									frm.set_value("total_amount", res.message[0][2]);
+									frm.set_value("items", res.message[1]);
+									frm.refresh();
+								}
+							})
+						}
+						else{
+							frappe.throw("Invoice is already returned");
+							frm.set_value("invoice_no",'');
+						}
+					}
+				}
+			})
+		}
+		else{
+			location.reload();
+		}
+	}
+});
 
-	// }
+frappe.ui.form.on('Sales Invoice Item', {
+	item_code:function(frm, cdt, cdn) {
+		var index = 0;
+        for(var i=frm.doc.items.length-1; i>=0;i--){
+            if(frm.doc.items[i].name == cdn){
+                index = i;
+                break;
+            }
+        }
+		if(!frm.doc.items[index].item_code){
+			frm.doc.items[index].qty = 0;
+			frm.doc.items[index]["item_name"] = '';
+			frm.doc.items[index]["rate"] = '';
+			frm.doc.items[index]['amount'] = '';			
+			frm.refresh_field("items");
+		}
+		else{
+			frm.doc.items[index].qty = 1;
+			frm.doc.items[index].amount = (parseFloat(frm.doc.items[index].qty) * parseFloat(frm.doc.items[index].rate) ).toFixed(2);
+			frm.refresh_field("items");
+			var total = 0;
+			for (var i = 0; i<frm.doc.items.length; i++){
+				total += parseFloat(frm.doc.items[i].amount);
+			}
+			frm.set_value("amount", total);
+			frm.set_value("total_amount", total - parseFloat(frm.doc.discount))
+			frm.refresh_field("amount");
+			frm.refresh_field("total_amount");
+		}
+	},
+	qty:function(frm, cdt, cdn){
+		var index = 0;
+        for(var i=frm.doc.items.length-1; i>=0;i--){
+            if(frm.doc.items[i].name == cdn){
+                index = i;
+                break;
+            }
+        }
+		frm.doc.items[index].amount = parseFloat(frm.doc.items[index].qty) * parseFloat(frm.doc.items[index].rate)
+		frm.refresh_field("items");
+		var total = 0;
+			for (var i = 0; i<frm.doc.items.length; i++){
+				total += parseFloat(frm.doc.items[i].amount);
+			}
+			frm.set_value("amount", total);
+			frm.set_value("total_amount", total - parseFloat(frm.doc.discount))
+			frm.refresh_field("amount");
+			frm.refresh_field("total_amount");
+	},
+	items_remove:function(frm, cdt, cdn){
+		console.log("HAI");
+		var total = 0;
+		for (var i = 0; i<frm.doc.items.length; i++){
+			total += parseFloat(frm.doc.items[i].amount);
+		}
+		frm.set_value("amount", total);
+		frm.set_value("total_amount", total - parseFloat(frm.doc.discount))
+		frm.refresh_field("amount");
+		frm.refresh_field("total_amount");
+	}
 });
--- skool/skool/doctype/sales_invoice/sales_invoice.py
+++ skool/skool/doctype/sales_invoice/sales_invoice.py
@@ -3,8 +3,40 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+@frappe.whitelist()
+def get_data(invoice):
+	return [frappe.db.get_value("Sales Invoice", {'name':invoice}, ['amount', 'discount', 'total_amount']), frappe.db.get_list("Sales Invoice Item", {'parent':invoice}, ['item_code', 'item_name', 'qty', 'rate', 'amount'])]
+
+@frappe.whitelist()
+def check_returned(invoice):
+	return frappe.db.exists("Sales Invoice", invoice+'-Returned')
 class SalesInvoice(Document):
-	pass
+	def autoname(self):
+		if self.invoice_no:
+			self.name = self.invoice_no+'-Returned'
+	def before_save(self):
+		if self.is_return:
+			if 'Returned' in self.invoice_no:
+				frappe.throw("Can not return a returned invoice")
+			if frappe.db.exists("Sales Invoice", self.invoice_no+'-Returned'):
+				frappe.throw("Invoice is already returned");
+		print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+		print(self.name, self.amount, self.discount, self.total_amount)
+		new_doc = frappe.new_doc("Stock Ledger")
+		new_doc.invoice_no = self.name
+		new_doc.invoice_type = 'Sales Invoice'
+		new_doc.amount = self.amount
+		new_doc.discount = self.discount
+		new_doc.total_amount = self.total_amount
+		for i in self.items:
+			print(i.__dict__)
+			new_row = new_doc.append("items", {})
+			new_row.item_code = i.item_code
+			new_row.item_name = i.item_name
+			new_row.qty = i.qty
+			new_row.rate = i.rate
+			new_doc.amount = i.amount
+		new_doc.save()
