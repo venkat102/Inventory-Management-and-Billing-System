126f860786d67659452a876ed3cd53d8c2bd1abb
venkat102 <venkatesharunachalam659@gmail.com>
2021-03-29 05:45:26 +0530

    fix

--- skool/stock/doctype/stock_ledger/stock_ledger.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Venkatesh and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Stock Ledger', {
-	// refresh: function(frm) {
-
-	// }
-});
